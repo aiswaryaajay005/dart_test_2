da548639ebbb4b7f4d4dd7cbb15fec26110958b5
aiswaryaajay005 <aiswaryaajayakumar176@gmail.com>
2025-01-29 12:35:24 +0530

    completed

--- bin/classes.dart
+++ bin/classes.dart
@@ -10,10 +10,11 @@ Create a base class named Shape with a private instance variable color.
  variables (color, radius, length, and width) in each class.
  */
 class Shape {
-  String? _color;
-
-  void displayColor(_color) {
-    print(_color);
+  String _color = 'Red';
+  String get getColor => _color;
+  set setColor(String newColor) => _color = newColor;
+  void displayColor() {
+    print("Color:$_color");
   }
 }
 
@@ -21,7 +22,7 @@ class Circle extends Shape {
   num _rad;
   Circle(this._rad);
   num get getRad => _rad;
-  void set setRad(num newrad) => _rad = newrad;
+  set setRad(num newrad) => _rad = newrad;
 }
 
 class Rectangle extends Shape {
@@ -29,11 +30,7 @@ class Rectangle extends Shape {
   num _breadth;
   Rectangle(this._length, this._breadth);
   num get getlen => _length;
-  void set setlength(num newlen) => _length = newlen;
+  set setlength(num newlen) => _length = newlen;
   num get getbre => _breadth;
-  void set setbreadth(num newbreadth) => _breadth = newbreadth;
+  set setbreadth(num newbreadth) => _breadth = newbreadth;
 }
-// void main() {
-//   Shape s = Shape();
-//   s.displayColor('Red');
-// }
--- bin/dart_test_2.dart
+++ bin/dart_test_2.dart
@@ -12,6 +12,7 @@ import 'dart:io';
 
 // void squareFunction(int num) {
 //   int n = num;
+//   print("Square of numbers from 1 to $n are:");
 //   for (int i = 1; i <= n; i++) {
 //     int sqr = i * i;
 //     stdout.write('$sqr ,');
@@ -60,10 +61,78 @@ import 'dart:io';
 //-------------------------------------------------------------------------
 /*Create a base class named Shape with a private instance variable color.
  Implement a method displayColor() within the Shape class to display the color.
-
  Next, create two derived classes, Circle and Rectangle, that inherit from the Shape class.
  The Circle class should have a private property radius, 
  and the Rectangle class should have private properties length and width. 
  Implement constructors for each derived class to initialize their respective properties.
  To demonstrate encapsulation, provide getter and setter methods for the private
  variables (color, radius, length, and width) in each class. */
+
+// import 'classes.dart';
+// void main() {
+//   Shape s = Shape();
+//   print("CLASS SHAPE");
+//   s.displayColor();
+//   print("Color initially: ");
+//   print(s.getColor);
+//   s.setColor = 'blue';
+//   print("Color after setting:");
+//   print(s.getColor);
+//   print("--------------------------------------------");
+//   print("CLASS CIRCLE");
+
+//   Circle c1 = Circle(3);
+//   c1.setColor = 'Green';
+//   print("Color of circle is ${c1.getColor}");
+//   print("Radius of circle initially:${c1.getRad}");
+//   c1.setRad = 5;
+//   print("After setting new Value to Radius:");
+//   print(c1.getRad);
+//   print("--------------------------------------------");
+//   print('CLASS RECTANGLE');
+//   Rectangle r1 = Rectangle(5, 2);
+//   r1.setColor = 'Yellow';
+//   print('Color of rectangle is ${r1.getColor}');
+//   print("Value of length and breadth initially");
+//   print("Length=${r1.getlen}");
+//   print("Breadth=${r1.getbre}");
+//   r1.setlength = 20;
+//   r1.setbreadth = 30;
+//   print("Value of length and breadth after setting");
+//   print("Length=${r1.getlen}");
+//   print("Breadth=${r1.getbre}");
+// }
+//---------------------------------------------------------------------------
+//---------------------------------------------------------------------------
+/* Define a mixin called Logger that has a method logMessage(String message) to print messages
+ to the console. Create an abstract class Vehicle with abstract methods startEngine() and
+ stopEngine(). Implement the Logger mixin in a class Car that extends Vehicle. Implement the
+ abstract methods to print log messages when the engine starts and stops.*/
+// mixin Logger {
+//   void logMessage(String messages) {
+//     print(messages);
+//   }
+// }
+
+// abstract class Vehicle {
+//   void startEngine();
+//   void stopEngine();
+// }
+
+// class Car extends Vehicle with Logger {
+//   @override
+//   void startEngine() {
+//     logMessage("Engine of the car is started");
+//   }
+
+//   @override
+//   void stopEngine() {
+//     logMessage("Engine of the car is stopped");
+//   }
+// }
+
+// void main() {
+//   Car c1 = Car();
+//   c1.startEngine();
+//   c1.stopEngine();
+// }
